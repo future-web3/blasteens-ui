522f3e5ab7215f5df9d1408c74a9bd1a5eececce
Wendy <xccxccxccb57@gmail.com>
2024-01-25 16:59:44 +1100

    refactor connect wallet

--- src/components/GameView/GameView.js
+++ src/components/GameView/GameView.js
@@ -1,25 +1,68 @@
-import { Outlet, Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "./GameView.module.scss";
 import React, { useEffect } from "react";
+import { useAccount, useConnect, useNetwork } from "wagmi";
+import events from "../../constants/events";
+
 import { gameGlossaryConfigs } from "../../configs/gameGlossaryConfig";
 import Phaser from "phaser";
 
 let game = null;
 
+export const emitter = new Phaser.Events.EventEmitter();
+
 function GameView() {
+  const { connectAsync, connect, connectors, error } = useConnect();
+  const { address, isConnected } = useAccount();
   const { gameId } = useParams();
 
   const targetGame = gameGlossaryConfigs[`${gameId}`];
+
+  useEffect(() => {
+    if (!isConnected) {
+      if (game) {
+        game.destroy(true);
+        game = null;
+      }
+      return;
+    }
+    if (!address) return;
+    if (game) return;
+
+    game = new Phaser.Game(targetGame.config);
+  }, [isConnected, address, game]);
 
   if (!targetGame) {
     return null;
   }
 
-  game = new Phaser.Game(targetGame.config);
+  const handleConnectWallet = async () => {
+    try {
+      if (!isConnected) {
+        connect({ connector: connectors[0] });
+      }
+    } catch (error) {
+      emitter.emit(events.AUTH_FAILED);
+    }
+  };
 
   return (
-    <div>
-      {targetGame.name}
+    <div className={styles.gameOuterContainer}>
+      {!isConnected && (
+        <div className={styles.filter}>
+          <div className={styles.buttonContainer}>
+            <button
+              className={styles.web3Button}
+              onClick={() => {
+                handleConnectWallet();
+              }}
+            >
+              Connect Your Wallet
+            </button>
+          </div>
+        </div>
+      )}
+      <p className={styles.gameTitle}>{targetGame.name}</p>
       <div id="game-glossary-frame"></div>
     </div>
   );
--- src/gameGlossary/germs/main.js
+++ src/gameGlossary/germs/main.js
@@ -1,18 +1,19 @@
-import Phaser from 'phaser';
-import Boot from './Boot.js';
-import Preloader from './Preloader.js';
-import MainMenu from './MainMenu.js';
-import MainGame from './Game.js';
+import Phaser from "phaser";
+import Boot from "./Boot.js";
+import Preloader from "./Preloader.js";
+import MainMenu from "./MainMenu.js";
+import MainGame from "./Game.js";
 
 export const germsConfig = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: '#000000',
-    parent: 'game-glossary-frame',
-    scene: [ Boot, Preloader, MainMenu, MainGame ],
-    physics: {
-        default: 'arcade',
-        arcade: { debug: false }
-    }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: "#000000",
+  parent: "game-glossary-frame",
+  autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+  scene: [Boot, Preloader, MainMenu, MainGame],
+  physics: {
+    default: "arcade",
+    arcade: { debug: false },
+  },
 };
