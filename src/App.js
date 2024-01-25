00515b4d30a26bccbcfed705d4f85ca14dad57fc
Jim Hsiao <is0.jimhsiao@gmail.com>
2024-01-25 12:51:34 +1100

    add games

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -7,17 +7,7 @@ function App() {
       <Route path="/" element={<Navbar />}>
         <Route index element={<div>Homepage</div>} />
         <Route path="about" element={<div>About Us</div>} />
-        <Route
-          path="game-glossary"
-          element={
-            <div>
-              Game Glossary
-              <div>
-                <Outlet />
-              </div>
-            </div>
-          }
-        >
+        <Route path="game-glossary">
           <Route path=":id" element={<div>ID Page</div>} />
         </Route>
         <Route path="crypto-dungeon" element={<div>Crypto Dungeon</div>} />
---
+++ src/gameGlossary/avoid the germs/Boot.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+
+export default class Boot extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('Boot');
+    }
+
+    preload ()
+    {
+        this.load.setPath('../../../public/assets/games/germs/');
+        this.load.image('background', 'background.png');
+        this.load.bitmapFont('slime', 'slime-font.png', 'slime-font.xml');
+    }
+
+    create ()
+    {
+        this.registry.set('highscore', 0);
+
+        this.scene.start('Preloader');
+    }
+}
