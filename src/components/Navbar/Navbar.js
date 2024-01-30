3025ac2dff95415c58711fd4b9528c0ba35ea379
Wendy <xccxccxccb57@gmail.com>
2024-01-30 12:49:44 +1100

    add connect button to nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,65 @@
-import { Outlet, Link } from "react-router-dom";
-import styles from "./Navbar.module.scss";
-import { FaWallet } from "react-icons/fa";
-import React from "react";
+import { Outlet, Link } from 'react-router-dom'
+import styles from './Navbar.module.scss'
+import { FaWallet } from 'react-icons/fa'
+import React from 'react'
+import { useAccount, useConnect, useNetwork } from 'wagmi'
 
 function Navbar() {
+  const { connect, connectors } = useConnect()
+  const { isConnected } = useAccount()
+
+  const handleConnectWallet = async () => {
+    try {
+      if (!isConnected) {
+        connect({ connector: connectors[0] })
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className={styles.navBarContainer}>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to='/about'>About</Link>
         </li>
         <li>
-          <Link to="/arcade/escape-from-germs">Germs</Link>
+          <Link to='/arcade/escape-from-germs'>Germs</Link>
         </li>
         <li>
-          <Link to="/arcade/tommy-jumping">Tom</Link>
+          <Link to='/arcade/tommy-jumping'>Tom</Link>
         </li>
         <li>
-          <Link to="/arcade/snowman-defender">Snowman</Link>
+          <Link to='/arcade/snowman-defender'>Snowman</Link>
         </li>
         <li>
-          <Link to="/arcade/emoji-match">Emoji</Link>
+          <Link to='/arcade/emoji-match'>Emoji</Link>
         </li>
         <li>
-          <div className={styles.walletIconContainer}>
-            <FaWallet />
-            Connect
-          </div>
+          {isConnected ? (
+            <div className={styles.avatarContainer}>
+              <img className={styles.avatar} src='/images/nft6.jpeg' />
+            </div>
+          ) : (
+            <div
+              className={styles.walletIconContainer}
+              onClick={() => {
+                handleConnectWallet()
+              }}
+            >
+              <FaWallet />
+            </div>
+          )}
         </li>
       </ul>
 
       <Outlet />
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
