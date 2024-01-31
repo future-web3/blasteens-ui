f5c4cf2314614e14288aefd9cd3044200fac2eb0
Jim Hsiao <is0.jimhsiao@gmail.com>
2024-01-31 11:05:07 +1100

    rename

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./Homepage.module.scss";
-import { gameListConfigs } from "../../configs/gameListConfig";
-import GameCard from "../../components/GameCard/GameCard";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Homepage.module.scss'
+import { gameListConfigs } from '../../configs/gameListConfig'
+import GameCard from '../../components/GameCard/GameCard'
 
 function Homepage() {
   return (
     <div className={styles.homepageContainer}>
       <div>123</div>
       <div></div>
-			<div></div>
+      <div></div>
     </div>
-  );
+  )
 }
 
-export default Homepage;
+export default Homepage
