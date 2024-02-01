9c1b326108363b3755db1c75beca4613ad1ebca8
itach-eth <itachieth@gmail.com>
2024-02-01 14:41:31 +1100

    fix not found style issue

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -5,12 +5,11 @@ import styles from './NotFound.module.scss'
 function NotFound() {
   return (
     <div className={styles.notFoundContainer}>
-      <div className={styles.notFoundSection} id='error-image'>
-        <img src='/images/blasteens-transparent.png' alt='Blasteens' />
+      <div className={styles.notFoundSection}>
+        <img src='/images/blasteens-transparent.png' alt='Blasteens' className={styles.errorImage} />
       </div>
-      <div className={styles.notFoundSection} id='error-text'>
-        <h1>oops...404</h1>
-        <h1>can't find the game for you :(</h1>
+      <div className={styles.notFoundSection}>
+        <h1>oops...404 <br /> can't find the game for you :(</h1>
       </div>
       <div className={styles.notFoundSection}>
         <Link to='/'>
