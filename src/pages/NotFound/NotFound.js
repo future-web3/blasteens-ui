3adb811d89514e953346f7180a152044cc945c05
Jim Hsiao <is0.jimhsiao@gmail.com>
2024-02-01 13:35:05 +1100

    modify not found

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -5,18 +5,16 @@ import styles from './NotFound.module.scss'
 function NotFound() {
   return (
     <div className={styles.notFoundContainer}>
-      <div className={styles.notFoundSection}>
+      <div className={styles.notFoundSection} id='error-image'>
         <img src='/images/blasteens-transparent.png' alt='Blasteens' />
       </div>
-      <div className={styles.notFoundSection}>
+      <div className={styles.notFoundSection} id='error-text'>
         <h1>oops...404</h1>
         <h1>can't find the game for you :(</h1>
       </div>
       <div className={styles.notFoundSection}>
         <Link to='/'>
-          <div className={styles.box}>
-            <p>Back Home</p>
-          </div>
+          <div className={styles.box}>Back Home</div>
         </Link>
       </div>
     </div>
@@ -24,9 +22,3 @@ function NotFound() {
 }
 
 export default NotFound
-
-{
-  /* <button>
-
-</button> */
-}
