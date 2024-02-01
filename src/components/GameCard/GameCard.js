0d7dcbd83ba1597b05831f92885c96420042c0dd
Jim Hsiao <is0.jimhsiao@gmail.com>
2024-02-01 19:03:15 +1100

    refine homepage

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -7,30 +7,38 @@ function GameCard({ gameId }) {
   const logoUrl = `/assets/games/${gameConfigs[gameId]['key']}/logo.png`
 
   return (
-    <div className={styles.gameCardContainer}>
-      <h2>{gameConfigs[gameId]['name']}</h2>
-      <div className={styles.gameCardButtonBox}>
-        {gameConfigs[gameId]?.ready ? (
-          <Link to={gameConfigs[gameId]?.url} className={styles.gameCardButton}>
-            PLAY
-          </Link>
-        ) : (
-          <span className={styles.gameCardButtonDisable}>Coming soon!</span>
-        )}
-      </div>
-      <div className={styles.gameCardLogo}>
-        <img src={logoUrl} alt='LOGO' />
-        <div className={styles.gameCardDescription}>{gameConfigs[gameId]['description']}</div>
-      </div>
-      <div className={styles.gameCardDetail}>
-        <p>
-          <span style={{ fontWeight: 'bold' }}>Prize Pool: </span>$2000
-          <br />
-          <span style={{ fontWeight: 'bold' }}>Highest Score: </span>923
-          <br />
-          <span style={{ fontWeight: 'bold' }}> Remaining: </span>01:23:12
-          <br />
-        </p>
+    <div className={styles.gameCardBox}>
+      <div className={styles.gameCardContainer}>
+        <h2>{gameConfigs[gameId]['name']}</h2>
+        <div className={styles.gameCardButtonBox}>
+          {gameConfigs[gameId]?.ready ? (
+            <Link to={gameConfigs[gameId]?.url} className={styles.gameCardButton}>
+              PLAY
+            </Link>
+          ) : (
+            <span className={styles.gameCardButtonDisable}>Coming soon!</span>
+          )}
+        </div>
+        <div className={styles.gameCardLogo}>
+          <img src={logoUrl} alt='LOGO' />
+          <div className={styles.gameCardDescription}>{gameConfigs[gameId]['description']}</div>
+        </div>
+        <div className={styles.gameCardDetail}>
+          <p>
+            <span className={styles.gameCardRow}>
+              <span style={{ fontWeight: 'bold' }}>Prize Pool</span>
+              <span>$2000</span>
+            </span>
+            <span className={styles.gameCardRow}>
+              <span style={{ fontWeight: 'bold' }}>Highest Score</span>
+              <span>923pt</span>
+            </span>
+            <span className={styles.gameCardRow}>
+              <span style={{ fontWeight: 'bold' }}>Remaining Time</span>
+              <span>01:23:12</span>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   )
