01eb0d62960e12a3a19d67a6e26236aa1306afdd
Wendy <xccxccxccb57@gmail.com>
2024-02-06 11:33:11 +1100

    use data on chain in prize page

--- src/pages/Prize/Prize.js
+++ src/pages/Prize/Prize.js
@@ -1,66 +1,122 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import styles from './Prize.module.scss'
 import Data from './testData.json'
+import { gameListConfigs } from '../../configs/gameListConfig'
+import { getABI, getContractAddress } from '../../helpers/network'
+import { checkScore } from '../../helpers/contracts'
+import { useBalance } from 'wagmi'
 
 export default function Price() {
-  const [isGamer, setIsGamer] = useState(true);
-  const [selected, setSelected] = useState(Data.data[0]);
-  const [isOpen, setIsOpen] = useState(false);
-  
+  const gameList = gameListConfigs.arcade
+
+  const [individuals, setIndividuals] = useState([])
+  const [isFetchingInfo, setIsFetchingInfo] = useState(false)
+  const [selected, setSelected] = useState(gameListConfigs.arcade[0])
+  const [isGamer, setIsGamer] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const gameContract = useMemo(() => {
+    const address = getContractAddress('GAME', 168587773, selected)
+    const abi = getABI('GAME')
+    if (!address || !abi) {
+      return null
+    }
+    return {
+      address,
+      abi
+    }
+  }, [selected])
+
+  const { data: poolPrize } = useBalance({ address: gameContract?.address })
+
   useEffect(() => {
-    const handleOutsideClick = (event) => {
-      if (isOpen && !event.target.closest(`.${styles.selectSection}`)) {
-        setIsOpen(false);
+    if (!gameContract || !selected) return
+    const leaderboardHandler = async () => {
+      try {
+        setIsFetchingInfo(true)
+        const data = await checkScore(gameContract, selected)
+        setIndividuals(data)
+      } catch (error) {
+        console.error('leaderboardHandler error', error.message)
+      } finally {
+        setIsFetchingInfo(false)
       }
-    };
-  
-    document.addEventListener('click', handleOutsideClick);
+    }
+    leaderboardHandler()
+  }, [gameContract, selected])
+
+  useEffect(() => {
+    const handleOutsideClick = event => {
+      if (isOpen && !event.target.closest(`.${styles.selectSection}`)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('click', handleOutsideClick)
     return () => {
-      document.removeEventListener('click', handleOutsideClick);
-    };
-  }, [isOpen]);
-
-
+      document.removeEventListener('click', handleOutsideClick)
+    }
+  }, [isOpen])
 
   return (
     <div>
-      <div className = {styles.page}>
+      <div className={styles.page}>
         <div className={styles.pricePage}>
-          <div className = {styles.contentWrapper}>
+          <div className={styles.contentWrapper}>
             <div className={styles.rankSection}>
-              <div className={styles.title}>{selected.Name}</div>
+              <div className={styles.title}>{selected}</div>
               <div className={styles.poolInfo}>
-                <div>Prize Pool:{Data.prizePool} blast</div>
+                <div>Prize Pool:{poolPrize?.formatted} blast</div>
                 <div>Claim Closed at: {Data.timeRemain}</div>
               </div>
+              {/*TODO:style issues*/}
               <ul className={styles.rankTable}>
-                  {Data.RankTable.map((item) => (
-                    <li className={styles.dataRow} key={item.rank}>
-                      <div className = {styles.data}>#{item.rank}</div>
-                      <div className = {styles.data}>{item.points}pt</div>
-                      <div className = {styles.data}>{item.address}</div>
-                      <div className = {styles.dataGreen}>$6</div>
-                      <div className = {styles.dataGreen}>10%</div>
-                    </li>
-                  ))}
+                {individuals.map(item => (
+                  <li className={styles.dataRow} key={item.rank}>
+                    <div className={styles.data}>#{item.rank}</div>
+                    <div className={styles.data}>{item.points}pt</div>
+                    <div className={styles.data}>{item.address}</div>
+                    {/*<div className={styles.dataGreen}>$6</div>*/}
+                    {/*<div className={styles.dataGreen}>10%</div>*/}
+                  </li>
+                ))}
               </ul>
             </div>
             <div className={styles.controlSection}>
               <div className={styles.controlButtons}>
                 <div className={styles.switch}>
-                  <div className= {isGamer ? styles.selected : styles.notSelected} onClick = {()=>setIsGamer(true)}>Gamer</div>
-                  <div className={isGamer ? styles.notSelected : styles.selected} onClick={() => setIsGamer(false)}>Developer</div>            </div>
+                  <div className={isGamer ? styles.selected : styles.notSelected} onClick={() => setIsGamer(true)}>
+                    Gamer
+                  </div>
+                  <div className={isGamer ? styles.notSelected : styles.selected} onClick={() => setIsGamer(false)}>
+                    Developer
+                  </div>{' '}
+                </div>
                 <div className={styles.selectSection}>
-                    <div className={isOpen?  styles.selectedNameOpen: styles.selectedName} onClick={() => setIsOpen(!isOpen)}>{selected.Name}</div>
-                    {isOpen && <div className={styles.dropdown}>
-                      {Data.data.map((item) => (
-                      <div className={styles.selectItem} onClick={() => {setSelected(item); setIsOpen(false)}}>{item.Name}</div>
-                    ))}
-                    </div>}
+                  <div className={isOpen ? styles.selectedNameOpen : styles.selectedName} onClick={() => setIsOpen(!isOpen)}>
+                    {selected}
+                  </div>
+                  {isOpen && (
+                    <div className={styles.dropdown}>
+                      {gameList.map(item => (
+                        <div
+                          className={styles.selectItem}
+                          onClick={() => {
+                            setSelected(item)
+                            setIsOpen(false)
+                          }}
+                        >
+                          {item}
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </div>
               </div>
               <div className={styles.claimSection}>
-                <div className = {styles.yieldText}>Your Yield:<div className={styles.textGreen}>1111 blast</div></div>
+                <div className={styles.yieldText}>
+                  Your Yield:<div className={styles.textGreen}>1111 blast</div>
+                </div>
                 <button className={styles.claimButton}>Claim</button>
               </div>
             </div>
@@ -68,6 +124,5 @@ export default function Price() {
         </div>
       </div>
     </div>
-   
   )
 }
