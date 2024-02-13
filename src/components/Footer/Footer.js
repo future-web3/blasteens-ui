7865370c7b1fe4bfe034a79c1950e230164f1b95
itach-eth <itachieth@gmail.com>
2024-02-13 18:04:00 +1100

    update logo + footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Footer.module.scss'
-import { FaMailBulk,FaTwitterSquare,FaLinkedin, FaTelegram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
 
 function Footer() {
   return (
@@ -8,19 +9,21 @@ function Footer() {
       <div className={styles.pictures}>
         <div className={styles.logo}> <img src='/images/scorpion-studio-transparent.png' alt='blasteen logo' className={styles.logoImg} /></div>
         <div className={styles.icons}>
-          <FaTwitterSquare className={styles.icon}/>
-          <FaLinkedin className={styles.icon}/>
-          <FaMailBulk className={styles.icon}/>
-          <FaTelegram className={styles.icon}/>
+          <a href='https://twitter.com/blasteens' target='_blank' rel="noreferrer">
+            <BsTwitterX className={styles.icon} />
+          </a>
+          <a href='https://github.com/future-web3/blasteens-ui' target='_blank' rel="noreferrer">
+            <FaGithub className={styles.icon} />
+          </a>
         </div>
       </div>
       <div className={styles.rightFooter}>
         <div className={styles.textContainer}>
-          <img src='/images/blast-logo-yellow.png' alt = 'blast logo' className={styles.intextPicture}></img>
+          <img src='/images/blast-logo-yellow.png' alt='blast logo' className={styles.intextPicture}></img>
           <div className={styles.text}>Blast is the only Ethereum L2 with native yield for ETH and stablecoins.</div>
         </div>
         <div className={styles.logo}><img src='/images/PowerByBlast.png' alt='power by blast' className={styles.logoImg} /></div>
-      </div> 
+      </div>
     </div>
   )
 }
